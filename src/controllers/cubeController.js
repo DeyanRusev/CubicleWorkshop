8157336c0f942e006d13e125d625bf041ceaffe8
Deyan Rusev <deyanrusev@gmail.com>
2023-05-22 23:08:03 +0300

    adding cubeManager

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
 
-router.get('/create', (req, res) => {
+const cubeManager = require('../menagers/cubeManager')
 
+router.get('/create', (req, res) => {
+    console.log(cubeManager.getAll());
     res.render('create');
 });
 
+router.post('/create', (req, res) => {
+
+    const {name, description, imageUrl, difficultyLevel} = req.body;
+
+    cubeManager.create({name, description, imageUrl, difficultyLevel: Number(difficultyLevel)})
+    res.redirect('/');
+})
 
 module.exports = router;
